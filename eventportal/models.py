f30d1ad0b87f3847742f14a06e4c14cee00d2aa2
Ajay Antu <ajaytechy7@gmail.com>
2022-07-07 21:36:44 +0530

    added fest create and view

--- eventportal/models.py
+++ eventportal/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from distutils.command.upload import upload
+from djongo import models
 
 # Create your models here.
+
+class Fest(models.Model):
+    fest_name = models.CharField(max_length=255)
+    fest_venue = models.CharField(max_length=255)
+    fest_img = models.ImageField(null=True,blank=True,upload_to="images/")
+
+
--- eventportal/views.py
+++ eventportal/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from .models import Fest
 # Create your views here.
+
+
+def fests(request):
+    if request.method=='POST':
+        fest_name = request.POST.get('fest_name')
+        fest_img = request.FILES.get('myfile')
+        venue = request.POST.get('venue')
+        if(fest_name and fest_img and venue):
+            Fest.objects.create(fest_name=fest_name,fest_venue=venue,fest_img=fest_img)
+        return redirect('/')
+    context = {'fests':Fest.objects.all()}
+    return render(request,'homepage/home.html',context);
